e86eaea3b36ceb0d380b04848d630eb961003926
HansithFonseka <hansithlochana@gmail.com>
2024-07-12 10:07:49 +0530

    questions and answers

---
+++ src/app/data.js
@@ -0,0 +1,61 @@
+export const quiz = {
+    totalQuestions: 9,
+    questions: [
+      {
+        id: 1,
+        question: 'Have you felt a lack of interest or pleasure in activities lately?',
+        answers: ['Not at all', 'Several days', 'More than half the days', 'Nearly every day'],
+     
+      },
+      {
+        id: 2,
+        question: 'Have you been feeling down, depressed, or hopeless recently?',
+        answers: ['Not at all', 'Several days', 'More than half the days', 'Nearly every day'],
+      
+      },
+      {
+        id: 3,
+        question: 'Have you had trouble sleeping or been sleeping too much?',
+        answers: ['Not at all', 'Several days', 'More than half the days', 'Nearly every day'],
+        
+      },
+      {
+        id: 4,
+        question: 'Have you felt unusually tired or low on energy?',
+        answers: ['Not at all', 'Several days', 'More than half the days', 'Nearly every day'],
+      
+      },
+      {
+        id: 5,
+        question: 'Have you felt unusually tired or low on energy?',
+        answers: ['Not at all', 'Several days', 'More than half the days', 'Nearly every day'],
+       
+      },
+      {
+        id: 6,
+        question: 'Have you been feeling bad about yourself or like you ve let yourself down?',
+        answers: ['Not at all', 'Several days', 'More than half the days', 'Nearly every day'],
+    
+      },
+      {
+        id: 7,
+        question: 'Have you had trouble concentrating on tasks?',
+        answers: ['Not at all', 'Several days', 'More than half the days', 'Nearly every day'],
+      
+      },
+      {
+        id: 8,
+        question: 'Have you been noticeably slower or more agitated in your movements lately?',
+        answers: ['Not at all', 'Several days', 'More than half the days', 'Nearly every day'],
+      
+      },
+      {
+        id: 9,
+        question: 'Have you had thoughts of self-harm or feeling that you would be better off dead?',
+        answers: ['Not at all', 'Several days', 'More than half the days', 'Nearly every day'],
+
+      }
+   
+    ],
+  };
+  
