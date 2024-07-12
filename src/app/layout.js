c2e40d8c2e206eef8b8082a0f944bb0acf1a496a
HansithFonseka <hansithlochana@gmail.com>
2024-07-12 10:11:48 +0530

    page js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
+import './globals.css'
+import './style.css'
+import './registerStyle.css'
+import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ["latin"] });
 
